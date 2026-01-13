a57ff01d6c8fc0caeac1cf114364736ebd74985d
Liquan Lei <liquanlei@Liquans-MacBook-Air.local>
2026-01-12 19:28:02 -0500

    deleted 1 function

--- localization.py
+++ localization.py
@@ -1,22 +1,7 @@
 import numpy as np
 import cv2
 
-def pixel_to_ray(pixel, K, plane_z=0.0, cam_pose_R=np.eye(3), cam_pose_t=np.zeros((3,1))):
-    """
-    Apparently this function is not needed at all so maybe just remove it.
-    Backproject a pixel px=(u,v) to a 3D point on a horizontal plane (z=plane_z) in world coordinates.
 
-    Arguments:
-      px: (u,v) pixel coordinates (tuple or 1x2 array)
-      K: 3x3 camera intrinsic matrix
-      plane_z: z coordinate of the plane in WORLD frame (meters)
-      cam_pose_R, cam_pose_t: camera rotation matrix (3x3) and translation (3x1).
-                           (i.e., world_point = R_cam_to_world @ cam_point + t_cam_to_world)
-
-    Returns:
-      X_world (3,) 3D point in world coordinates lying on plane_z (if intersection exists)
-    """
-    return
 
 def triangulate_two_rays(pt1, pt2, K1, K2, R1=np.eye(3), t1=np.zeros((3,1)), R2=np.eye(3), t2=np.zeros((3,1)), dist1=None, dist2=None):
     """
@@ -31,7 +16,7 @@ def triangulate_two_rays(pt1, pt2, K1, K2, R1=np.eye(3), t1=np.zeros((3,1)), R2=
                            (i.e., world_point = R_cam_to_world @ cam_point + t_cam_to_world)
 
     Returns:
-      X_world (3,) 3D point in world coordinates lying on plane_z (if intersection exists)
+      final (3,) 3D point in world coordinates lying on plane_z (if intersection exists)
     """
     RT1=np.hstack([R1,t1]) #Combine matrices sideways
     RT2=np.hstack([R2,t2]) 
